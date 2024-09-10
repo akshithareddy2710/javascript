2f75139b6c8ec664c8b238eae899d0b5503edba4
akshithareddy2710 <gudakrishnavenireddy@gmail.com>
2024-09-10 22:46:02 +0530

    commit 1

---
+++ compareSummary.js
@@ -0,0 +1,33 @@
+const score = 100
+const scorevalue = 99.4
+
+const isloggedIn = true 
+const insideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log (id === anotherId);
+
+//const bigNumber = 22333444455555n
+
+console.log (typeof bigNumber)
+
+const department = ["cse", "ece" , "phe"];
+let myObj = {
+    name : "laya"
+
+}
+
+
+
+
+const myFunction = function(){
+    console.log("this is akshitha");
+}
+
+console.log(typeof myFunction);
+console.log(typeof anotherId);
+console.log(typeof outsideTemp );
+console.log(typeof scorevalue);
+console.log(typeof department );
