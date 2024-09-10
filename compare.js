61a303381cfa2d323fb96c6121113f4f70b53a32
akshithareddy2710 <gudakrishnavenireddy@gmail.com>
2024-09-10 21:56:42 +0530

    commit

---
+++ compare.js
@@ -0,0 +1,14 @@
+console.log(2==4);
+console.log(2>5);
+console.log(3==4);
+
+console.log("3">1)
+console.log("03">1)
+
+console.log(null == 0)
+console.log(null > 0)
+console.log(null < 0)
+
+console. log(undefined == 0);
+console. log(undefined > 0);
+console .log(undefined < 0);
---
+++ stringToNumConversion.js
@@ -0,0 +1,21 @@
+
+let str1 =" hello"
+let str2 = " world "
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+let gamechanger = 100
+++gamechanger;
+console.log(gamechanger);
